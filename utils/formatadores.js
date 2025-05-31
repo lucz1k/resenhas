7d608608fc276883f5c7a8b90e39e099b7538321
lucz1k <lucas_artigiani@outlook.com>
2025-05-31 04:31:03 -0300

    Adiciona formatadores

---
+++ utils/formatadores.js
@@ -0,0 +1,31 @@
+// utils/formatadores.js
+
+export function formatarPelotao(valor) {
+  return valor.replace(/\d+/, (num) => `${num}º`).replace(/pel/i, 'Pelotão');
+}
+
+export function formatarCia(valor) {
+  return valor.replace(/\d+/, (num) => `${num}ª`).replace(/cia/i, 'Cia');
+}
+
+export function formatarPosto(nome) {
+  return nome
+    .replace(/SD PM/i, 'Cb PM')
+    .replace(/CB PM/i, 'Cb PM')
+    .replace(/3º SGT PM/i, '3º Sgt PM')
+    .replace(/2º SGT PM/i, '2º Sgt PM')
+    .replace(/1º SGT PM/i, '1º Sgt PM')
+    .replace(/SUBTEN PM/i, 'Subten PM')
+    .replace(/Al Of PM/i, 'Al Of PM')
+    .replace(/ASP OF PM/i, 'Asp Of PM')   
+    .replace(/2º TEN PM/i, '2º Ten PM')
+    .replace(/1º TEN PM/i, '1º Ten PM')
+    .replace(/CAP PM/i, 'Cap PM')     
+    .replace(/MAJ PM/i, 'Maj PM')
+    .replace(/TEN CEL PM/i, 'Ten Cel PM')
+    .replace(/CEL PM/i, 'Cel PM');
+}
+
+export function removerCaracteresEspeciais(texto) {
+  return texto.replace(/[^\w\s]/gi, '').trim();
+}
